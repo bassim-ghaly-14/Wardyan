0bf30aebef19ffd2ffbe3f5e315f05cb628f944f
Bassim Ghaly <bassim.ghaly@gmail.com>
2026-09-06 12:56:04 +0300

    fix(security): harden email and phone validation regexes

--- js/core/utils.js
+++ js/core/utils.js
@@ -4,15 +4,22 @@ export function formatPrice(price) {
 
 export function createElement(tag, className, html) {
   const el = document.createElement(tag);
-  if (className) el.className = className;
-  if (html) el.innerHTML = html;
+
+  if (className) {
+    el.className = className;
+  }
+
+  if (html) {
+    el.innerHTML = html;
+  }
+
   return el;
 }
 
 export function validateEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  return /^[^@\s]{1,64}@[^@\s]{1,255}\.[^@\s]{2,63}$/.test(email);
 }
 
 export function validatePhone(phone) {
-  return /^01[0-2,5][0-9]{8}$/.test(phone);
+  return /^01[0-25][0-9]{8}$/.test(phone);
 }
